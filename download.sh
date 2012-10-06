5d7b193b8e447319dced2e7f1c9b052e21fb9a59
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-10-06 15:55:28 +0300

    download script for data update

---
+++ download.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget -O data/hotels.csv 'https://www.google.com/fusiontables/api/query?sql=select * from 1UpivIonjSpBuMy1ZaKobsI0a2rv13U4mjGhbNCk'
+wget -O data/halls.csv 'https://www.google.com/fusiontables/api/query?sql=select * from 1eyIhxlHGfHWH_WQGWeX7JiJhWatmP2TMYPHIGAs'
