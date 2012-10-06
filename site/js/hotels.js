87bd823d7e4e0dd97c2baa71285d4b0ebf766323
Kęstutis Malinauskas <kmalin@gmail.com>
2012-10-06 19:00:53 +0300

    Search filter UI form.

--- site/js/hotels.js
+++ site/js/hotels.js
@@ -10,3 +10,74 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+$(function() {
+  
+  var $sidebar = $('#sidebar');
+  
+  var filter = function(model) {
+    console.log(model);
+  };
+  
+  var readRange = function($slider) {
+    return [$slider.slider( "values", 0 ), $slider.slider( "values", 1 )];
+  };
+  
+  var executeFilter = function() {
+    
+    setTimeout(function(){
+      var model = {};
+      console.log('execute filter');
+      model.type = $sidebar.find('.model-type .active').data('type');
+      $sidebar.find('.model-hall').toggle(model.type == 'hall');
+      $sidebar.find('.model-hotel').toggle(model.type == 'hotel');
+      model.forDisabled = $sidebar.find('.model-for-disabled').is(':checked');
+      
+      model.vacancies = readRange($sidebar.find('.model-vacancy-count'));
+      model.grades = [];
+      $sidebar.find('.model-grade .btn').each(function() {
+        var $this = $(this);
+        if ($this.hasClass('active')) {
+          model.grades.push($this.data('value'));
+        }
+      });
+      
+      
+      
+      filter(model);
+    }, 1);   
+  };
+  
+  var toggleAndExecute = function() {
+    $(this).toggleClass('active');
+    executeFilter();
+  };
+  
+  var toggleChildrenAndExecute = function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    executeFilter();
+  };
+  $sidebar.find('.model-type .btn').click(toggleChildrenAndExecute);
+  
+  $sidebar.find('.model-for-disabled').change(executeFilter);
+  
+  $sidebar.find('.model-grade .btn').click(toggleAndExecute);
+
+  $sidebar.find('.model-vacancy-count').slider({
+    range: true,
+    min: 0,
+    max: 100,
+    values: [ 10, 80 ],
+    slide: executeFilter
+  });
+  
+  $sidebar.find('.model-needed-count').slider({
+    min: 0,
+    max: 100,
+    values: [ 50 ],
+    slide: executeFilter
+  });
+  
+  //first execution
+  executeFilter();
+});
